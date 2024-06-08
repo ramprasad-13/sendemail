393c2769a353ef616a3abb22da59165e74cbf6f5
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2024-06-08 15:47:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
 var corsOptions = {
-    origin: 'https://abroad.vercel.app',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST','PATCH','DELETE'], // Specify your origin here
     credentials: true,  // This allows the session cookie to be sent back and forth
     //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
